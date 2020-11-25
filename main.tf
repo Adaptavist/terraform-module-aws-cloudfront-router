7aaa0db36b1f2b49dba15ba9dcad7e16e4ea900e
Dominic Martino <dmartino@adaptavist.com>
2020-11-25 13:14:23 +0000

    initial version of module

---
+++ main.tf
@@ -0,0 +1,113 @@
+locals {
+
+  stageTag = {
+    "Avst:Stage:Name" = var.stage
+  }
+
+  finalTags = merge(var.tags, local.stageTag)
+}
+
+module "labels" {
+  source  = "cloudposse/label/terraform"
+  version = "0.5.0"
+
+  namespace = var.namespace
+  stage     = var.stage
+  name      = var.name
+  tags      = local.finalTags
+}
+
+resource "aws_cloudfront_distribution" "this" {
+
+  dynamic origin {
+    for_each = var.origin_mappings
+    content {
+      domain_name = origin.value.domain_name
+      origin_id   = origin.value.origin_id
+
+      custom_origin_config {
+        http_port              = 80
+        https_port             = 443
+        origin_protocol_policy = "https-only"
+        origin_ssl_protocols   = ["SSLv3", "TLSv1.2"]
+      }
+    }
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "Cloudfront distribution for ${module.labels.id}"
+  default_root_object = var.default_root_object
+
+  aliases = var.aliases
+
+  default_cache_behavior {
+    allowed_methods  = var.default_cache_behavior.allowed_methods
+    target_origin_id = var.default_cache_behavior.origin_id
+
+    viewer_protocol_policy = var.viewer_protocol_policy
+
+    forwarded_values {
+      query_string = true
+
+      cookies {
+        forward = "all"
+      }
+    }
+
+    // We are using cloudfront for routing only, we dont want to cache anything. 
+    min_ttl     = 0
+    default_ttl = 0
+    max_ttl     = 0
+    // we have to specifiy some methods
+    cached_methods = ["HEAD", "GET"]
+
+  }
+
+  dynamic ordered_cache_behavior {
+    for_each = var.origin_mappings
+    content {
+
+      path_pattern     = ordered_cache_behavior.value.path_pattern
+      allowed_methods  = ordered_cache_behavior.value.allowed_methods
+      target_origin_id = ordered_cache_behavior.value.origin_id
+
+      forwarded_values {
+        query_string = true
+
+
+        cookies {
+          forward = "all"
+        }
+      }
+
+      compress               = true
+      viewer_protocol_policy = var.viewer_protocol_policy
+
+      // We are using cloudfront for routing only, we dont want to cache anything. 
+      min_ttl     = 0
+      default_ttl = 0
+      max_ttl     = 0
+      // we have to specifiy some methods
+      cached_methods = ["HEAD", "GET"]
+
+    }
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  price_class = "PriceClass_All"
+
+  tags = module.labels.tags
+
+  viewer_certificate {
+    acm_certificate_arn      = var.acm_cert_arn
+    minimum_protocol_version = "TLSv1.2_2018"
+    ssl_support_method       = "sni-only"
+  }
+}
+
---
+++ test/fixture/main.tf
@@ -0,0 +1,96 @@
+/*
+  Test Fixture
+*/
+
+terraform {
+  backend "s3" {
+    bucket         = "product-sandbox-terraform-state-management"
+    dynamodb_table = "product-sandbox-terraform-state-management"
+    region         = "us-east-1"
+    encrypt        = "true"
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+locals {
+  default_allowed_methods = ["HEAD", "GET", "OPTIONS"]
+  tld                     = "avst-sbx.adaptavist.com"
+  domain                  = "${random_string.random.result}-cf-router-test.${local.tld}"
+  namespace               = "tf-tests"
+  stage                   = "test"
+  name                    = "cf-router"
+  tags = {
+    "Avst:BusinessUnit" : "platform"
+    "Avst:Team" : "cloud-infra"
+    "Avst:CostCenter" : "foo"
+    "Avst:Project" : "foo"
+    "Avst:Stage:Type" : "sandbox"
+  }
+}
+
+data "aws_acm_certificate" "cert" {
+  domain   = "*.${local.tld}"
+  statuses = ["ISSUED"]
+}
+
+data "aws_route53_zone" "zone" {
+  name         = local.tld
+  private_zone = false
+}
+
+resource "random_string" "random" {
+  length  = 8
+  special = false
+  upper   = false
+}
+
+module cf_distro {
+  source = "../../"
+
+  aws_region = var.aws_region
+  aliases    = [local.domain]
+
+  namespace = local.namespace
+  stage     = local.stage
+  name      = local.name
+  tags      = local.tags
+
+  acm_cert_arn = data.aws_acm_certificate.cert.arn
+
+  default_cache_behavior = {
+    origin_id       = "bbc"
+    domain_name     = "www.bbc.co.uk"
+    allowed_methods = local.default_allowed_methods
+  }
+
+  origin_mappings = {
+    scriptrunner = {
+      origin_id       = "scriptrunner"
+      domain_name     = "scriptrunner.adaptavist.com"
+      path_pattern    = "/latest/*"
+      allowed_methods = local.default_allowed_methods
+    }
+    bbc = {
+      origin_id       = "bbc"
+      domain_name     = "www.bbc.co.uk"
+      path_pattern    = "/news/*"
+      allowed_methods = local.default_allowed_methods
+    }
+
+  }
+}
+
+resource "aws_route53_record" "www" {
+  zone_id = data.aws_route53_zone.zone.zone_id
+  name    = local.domain
+  type    = "A"
+
+  alias {
+    name                   = module.cf_distro.cf_domain_name
+    zone_id                = module.cf_distro.cf_hosted_zone_id
+    evaluate_target_health = false
+  }
+}
